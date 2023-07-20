cf1f5ab337300f0fe6a210e573a8671439a1d300
Hamzah Diza Santoso <hamzah.diza@gmail.com>
2023-07-20 13:27:59 +0700

    update: gabung semua card ke main report card

--- custom-components/components/main-report-card/main-report-card.js
+++ custom-components/components/main-report-card/main-report-card.js
@@ -1,16 +1,27 @@
-//  <main-menu-card color="" backgroundColor="" width="" height="" value1= "Cash" value2="1.78%" value3="$ 6.899.00" value4="23/10/2023 12:45" link=">>>Link<<<"></main-menu-card>
+// <main-menu-card type="primary" color="" backgroundColor="" width="" height="" value1= "Cash" value2="1.78%" value3="$ 6.899.00" value4="23/10/2023 12:45" url=">>>url<<<"></main-menu-card>
+// <main-menu-card type="secondary" themeIcon="green" color="" backgroundColor="" width="" height="" value1="Income" value2="16.100.000" url=">>>url<<<" iconImage="https://cdn-icons-png.flaticon.com/512/2474/2474450.png"></main-menu-card>
+
 Component({
   mixins: [],
   data: {
+    handleIcon : "",
+    classContainer: "",
     width: "", 
     height: "", 
-    backgroundColor: ""
+    backgroundColor: "",
+    fontSizeValue1: 24, 
+    fontSizeValue2: 28, 
+    imageSize: 65
   },
   props: {},
   didMount() {
+    this.handleIcon()
     this.classContainer()
+    this.setDynamicFontSize();
   },
-  didUpdate() {},
+  didUpdate() {
+    // this.setDynamicFontSize();
+  },
   didUnmount() {},
   methods: {
     handleTap() {
@@ -25,6 +36,52 @@ Component({
         backgroundColor: backgroundColor,
         color: color
       });
-     }
+     },
+
+     handleIcon() {
+      let iconImage = ""
+      let isImageUrl = ""
+      if(this.props.iconImage){
+        iconImage = this.props.iconImage;
+        isImageUrl = iconImage.indexOf("http://") == 0 || iconImage.indexOf("https://") == 0
+      }
+    
+      if (isImageUrl) {
+        this.setData({
+          handleIcon: iconImage
+        });
+      } else {
+        this.setData({
+          handleIcon: iconImage
+        });
+      }
+    },
+
+    setDynamicFontSize() {
+      const query = this.createSelectorQuery();
+      query.select('.card-secondary').boundingClientRect();
+      query.exec(res => {
+        console.log(res[0] ? res[0].width : 0, "<<<< WIDTH");
+        const cardWidth = res[0] ? res[0].width : 0;
+
+        // const newFontSizeValue1 = Math.max(Math.min(cardWidth / 20, 28), 18); 
+        // const newFontSizeValue2 = newFontSizeValue1 + 4; 
+        let newFontSizeValue1;
+        let newImageSize;
+        if(cardWidth > 250 ) {
+          newFontSizeValue1 = 34; 
+          newImageSize = 100
+        }
+        let newFontSizeValue2 = newFontSizeValue1 + 4; 
+
+        console.log(newFontSizeValue1, newFontSizeValue2);
+
+        this.setData({
+          fontSizeValue1: newFontSizeValue1,
+          fontSizeValue2: newFontSizeValue2,
+          imageSize: newImageSize
+        });
+      });
+    },
   },
 });
