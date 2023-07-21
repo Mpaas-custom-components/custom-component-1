dcd93612316ecd43f0eebbc4b1c59aa02548e778
unknown <BNI@DGL-64176-1.hq.bni.co.id>
2023-07-21 14:32:17 +0700

    feat: dropdown-action

--- custom-components/pages/index/index.js
+++ custom-components/pages/index/index.js
@@ -1,2 +1,38 @@
 Page({
+  data: {
+    items: [{
+      name: 'angular',
+      value: 'AngularJS',
+    },
+    {
+      name: 'react',
+      value: 'React'
+    },
+    {
+      name: 'polymer',
+      value: 'Polymer'
+    },
+    {
+      name: 'vue',
+      value: 'Vue.js'
+    },
+    {
+      name: 'ember',
+      value: 'Ember.js'
+    },
+  ],
+  dropDownValue : ""
+  },
+
+  handleSubmit(e){
+    my.alert({
+      content: this.data.dropDownValue
+    })
+  },
+
+  handleValue(data){
+    this.setData({
+      dropDownValue : data
+    })
+  }
 });
