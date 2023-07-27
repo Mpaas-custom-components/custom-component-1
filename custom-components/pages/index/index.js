39274dc8a21a12160a1189cede8e75606c58239b
unknown <BNI@DGL-64176-1.hq.bni.co.id>
2023-07-27 16:26:07 +0700

    feat: Dropdown & Pages

--- custom-components/pages/index/index.js
+++ custom-components/pages/index/index.js
@@ -1,1 +1,17 @@
-Page({})
+Page({
+  data: {
+    formData: "",
+    items: [
+      { name: 'angular', value: 'AngularJS' },
+      { name: 'react', value: 'React' },
+      { name: 'polymer', value: 'Polymer' },
+      { name: 'vue', value: 'Vue.js' },
+      { name: 'ember', value: 'Ember.js' },
+      { name: 'backbone', value: 'Backbone.js' },
+    ],
+  },
+
+  handlesubmit(e){
+    console.log(e.detail.value.dropdown)
+  }
+})
