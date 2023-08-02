9101e36290f83783ee115db58aabec5d39252f50
unknown <BNI@DGL-64176-1.hq.bni.co.id>
2023-08-02 14:02:48 +0700

    Feat: cash Page

--- custom-components/components/dropdown-action/dropdown-action.js
+++ custom-components/components/dropdown-action/dropdown-action.js
@@ -1,5 +1,3 @@
-
-
 Component({
   mixins: [],
   data: {
@@ -9,37 +7,89 @@ Component({
     scrollVisible: false,
     closeVisible: false,
     valueData: "",
-    months: [
-      { month: 'January', number: 1 },
-      { month: 'February', number: 2 },
-      { month: 'March', number: 3 },
-      { month: 'April', number: 4 },
-      { month: 'May', number: 5 },
-      { month: 'June', number: 6 },
-      { month: 'July', number: 7 },
-      { month: 'August', number: 8 },
-      { month: 'September', number: 9 },
-      { month: 'October', number: 10 },
-      { month: 'November', number: 11 },
-      { month: 'December', number: 12 },
+    months: [{
+        month: 'January',
+        number: 1
+      },
+      {
+        month: 'February',
+        number: 2
+      },
+      {
+        month: 'March',
+        number: 3
+      },
+      {
+        month: 'April',
+        number: 4
+      },
+      {
+        month: 'May',
+        number: 5
+      },
+      {
+        month: 'June',
+        number: 6
+      },
+      {
+        month: 'July',
+        number: 7
+      },
+      {
+        month: 'August',
+        number: 8
+      },
+      {
+        month: 'September',
+        number: 9
+      },
+      {
+        month: 'October',
+        number: 10
+      },
+      {
+        month: 'November',
+        number: 11
+      },
+      {
+        month: 'December',
+        number: 12
+      },
     ],
     years: [],
+    tagData: [{
+        name: "1",
+        value: "1"
+      },
+      {
+        name: "2",
+        value: "2"
+      },
+      {
+        name: "3",
+        value: "3"
+      }
+    ],
+    tagTemp: {
+      name: "",
+      value: ""
+    }
   },
-  props: {
-  },
+  props: {},
   didMount() {
     this.getYearsFrom(1945)
-    if(this.props.type === "dateMonth"){
+    if (this.props.type === "dateMonth") {
       this.getCurrentMonth()
-    }else if(this.props.type === "dateYear"){
+    } else if (this.props.type === "dateYear") {
       this.setData({
         valueData: new Date().getFullYear()
       })
-    }else{
+    } else if (!this.props.defaultValue) {
       this.setData({
         valueData: "Filter"
       })
     }
+
   },
   didUpdate() {},
   didUnmount() {},
@@ -78,30 +128,62 @@ Component({
       })
     },
 
+    tagChange(e){
+      this.setData({
+        valueData: e.detail.value
+      })
+    },
+
     getYearsFrom(startYear) {
       const currentYear = new Date().getFullYear();
       const years = [];
-    
+
       for (let year = startYear; year <= currentYear; year++) {
         years.push(year);
       }
-    
+
       this.setData({
         years: years
       })
     },
 
-    getCurrentMonth(){
+    getCurrentMonth() {
       const month = new Date().getMonth().toString()
-      
+
       this.data.months.map((el) => {
-        if(el.number - 1 == month){
+        if (el.number - 1 == month) {
           this.setData({
-            valueData : el.month
+            valueData: el.month
           })
         }
       })
+    },
+
+    onChangeInput(e) {
+      this.setData({
+        tagTemp: {
+          name : e.detail.value,
+          value: e.detail.value
+        }
+      })
+    },
+
+    onSaveTag() {
+      let lastData = [...this.data.tagData]
+      
+      lastData.push(this.data.tagTemp)
+
+      
+      this.setData({
+        tagData: lastData,
+        basicVisible: false,
+        scrollVisible: false,
+        closeVisible: false,
+      })
+
+      console.log(this.data.tagData)
     }
   },
-  
+
+
 });
